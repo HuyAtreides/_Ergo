aa024394987307617ee460c4269a3c95ece07893
Nguyen Thinh <quocthinhmyt@gmail.com>
2024-11-23 11:27:52 +0700

    #24 Bo sung Service

---
+++ src/main/java/cnpm/ergo/service/implement/BlogServiceImpl.java
@@ -0,0 +1,52 @@
+package cnpm.ergo.service.implement;
+
+import java.util.List;
+import cnpm.ergo.service.interfaces.IBlogService;
+import cnpm.ergo.DAO.implement.BlogDaoImpl;
+import cnpm.ergo.DAO.interfaces.IBlogDao;
+import cnpm.ergo.entity.Blog;
+
+public class BlogServiceImpl implements IBlogService {
+
+    private final IBlogDao blogDao = new BlogDaoImpl();
+
+    @Override
+    public void addBlog(Blog blog) {
+        blogDao.insert(blog);
+    }
+
+    @Override
+    public void updateBlog(Blog blog) {
+        blogDao.update(blog);
+    }
+
+    @Override
+    public void deleteBlog(int blogId) {
+        try {
+            blogDao.delete(blogId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error deleting blog with ID: " + blogId);
+        }
+    }
+
+    @Override
+    public Blog getBlogById(int blogId) {
+        return blogDao.findById(blogId);
+    }
+
+    @Override
+    public List<Blog> getAllBlogs() {
+        return blogDao.findAll();
+    }
+
+    @Override
+    public List<Blog> searchBlogsByTitle(String title) {
+        return blogDao.searchByTitle(title);
+    }
+
+    @Override
+    public int getBlogCount() {
+        return blogDao.count();
+    }
+}
---
+++ src/main/java/cnpm/ergo/service/interfaces/IBlogService.java
@@ -0,0 +1,14 @@
+package cnpm.ergo.service.interfaces;
+
+import java.util.List;
+import cnpm.ergo.entity.Blog;
+
+public interface IBlogService {
+    void addBlog(Blog blog);
+    void updateBlog(Blog blog);
+    void deleteBlog(int blogId);
+    Blog getBlogById(int blogId);
+    List<Blog> getAllBlogs();
+    List<Blog> searchBlogsByTitle(String title);
+    int getBlogCount();
+}
