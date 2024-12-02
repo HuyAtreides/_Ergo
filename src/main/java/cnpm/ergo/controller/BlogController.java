a5a23bcaacfe7b8dfbede39a6cee9a7f012ef0a1
HiIamCuong <nguyensycuonglop10a12ntt@gmail.com>
2024-12-02 11:43:58 +0700

    BlogController

---
+++ src/main/java/cnpm/ergo/controller/BlogController.java
@@ -0,0 +1,30 @@
+package cnpm.ergo.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import cnpm.ergo.entity.Blog;
+import cnpm.ergo.service.implement.BlogServiceImpl;
+import cnpm.ergo.service.interfaces.IBlogService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns = {"/blog"})
+public class BlogController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	public IBlogService blogService = new BlogServiceImpl();
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String url = req.getRequestURI();
+		if(url.contains("/blog"))
+		{
+			List<Blog> list = blogService.getAllBlogs();
+
+			req.setAttribute("listblog", list);
+
+			req.getRequestDispatcher("/customer/blog-list.jsp").forward(req, resp);
+		}
+	}
+}
