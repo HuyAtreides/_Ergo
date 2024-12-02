4266a196833126754b2da4b1da92422f4d19fe68
HiIamCuong <nguyensycuonglop10a12ntt@gmail.com>
2024-12-02 23:44:54 +0700

    Them chuc nang tim kiem va chi tiet blog

--- src/main/java/cnpm/ergo/controller/BlogController.java
+++ src/main/java/cnpm/ergo/controller/BlogController.java
@@ -11,31 +11,45 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-@WebServlet(urlPatterns = {"/customer/blog"})
+@WebServlet(urlPatterns = {"/customer/blogs","/customer/blog/details","/customer/blog/search"})
 public class BlogController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	public IBlogService blogService = new BlogServiceImpl();
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String url = req.getRequestURI();
-		if(url.contains("/customer/blog"))
+		if(url.contains("/customer/blogs"))
 		{
 			List<Blog> list = blogService.getAllBlogs();
 
 			req.setAttribute("listblog", list);
 
-			req.getRequestDispatcher("/customer/blog-list.jsp").forward(req, resp);
+			req.getRequestDispatcher("/customer/views/blog-list.jsp").forward(req, resp);
 		}
-		else if (url.contains("/customer/blog/edit")) {
+		else if (url.contains("/customer/blog/details")) {
 
 			int id = Integer.parseInt(req.getParameter("id"));
 
 			Blog blog = blogService.getBlogById(id);
 
-			req.setAttribute("cate", blog);
+			req.setAttribute("blog", blog);
 
-			req.getRequestDispatcher("/customer/detail-blog.jsp").forward(req, resp);
+			req.getRequestDispatcher("/customer/views/blog-details.jsp").forward(req, resp);
 
+		}
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String url = req.getRequestURI();
+		if(url.contains("/customer/blog/search"))
+		{
+			String title = req.getParameter("title");
+			
+			List<Blog>list=blogService.searchBlogsByTitle(title);
+			
+			req.setAttribute("listblog", list);
+			
+			req.getRequestDispatcher("/customer/views/blog-list.jsp").forward(req, resp);
 		}
 	}
 }
