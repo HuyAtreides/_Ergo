d1f921702732ea573796b51801d4bf01c82da1e8
HiIamCuong <nguyensycuonglop10a12ntt@gmail.com>
2024-12-02 17:17:54 +0700

    BlogController

--- src/main/java/cnpm/ergo/controller/BlogController.java
+++ src/main/java/cnpm/ergo/controller/BlogController.java
@@ -11,20 +11,31 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-@WebServlet(urlPatterns = {"/blog"})
+@WebServlet(urlPatterns = {"/customer/blog"})
 public class BlogController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	public IBlogService blogService = new BlogServiceImpl();
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String url = req.getRequestURI();
-		if(url.contains("/blog"))
+		if(url.contains("/customer/blog"))
 		{
 			List<Blog> list = blogService.getAllBlogs();
 
 			req.setAttribute("listblog", list);
 
 			req.getRequestDispatcher("/customer/blog-list.jsp").forward(req, resp);
+		}
+		else if (url.contains("/customer/blog/edit")) {
+
+			int id = Integer.parseInt(req.getParameter("id"));
+
+			Blog blog = blogService.getBlogById(id);
+
+			req.setAttribute("cate", blog);
+
+			req.getRequestDispatcher("/customer/detail-blog.jsp").forward(req, resp);
+
 		}
 	}
 }
