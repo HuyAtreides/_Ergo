8828635a4ad471534f69dbfc0ce2b6a57e732abe
DOCUTEE <110187691+DOCUTEE@users.noreply.github.com>
2024-12-03 10:28:08 +0700

    DEVPHUCCART

--- src/main/java/cnpm/ergo/controller/customer/CustomerHomeController.java
+++ src/main/java/cnpm/ergo/controller/customer/CustomerHomeController.java
@@ -1,4 +1,4 @@
-package cnpm.ergo.controller.customer;
+package cnpm.ergo.controller.Customer;
 
 import cnpm.ergo.service.implement.CategoryServiceImpl;
 import cnpm.ergo.service.implement.MarketingCampaignServiceImpl;
